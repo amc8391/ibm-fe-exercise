a7c3d4b2432db213aec24dc7132596359cc7c685
Alex Michael Christodoulou Jr <amc8391@rit.edu>
2018-07-24 06:30:48 -0400

    Implemented Company Cost in API
    There may be a faster way to do this through a query, but I wanted to
    get an initial implementation into place and check on that later

--- loopback-api/common/models/company.js
+++ loopback-api/common/models/company.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Company) {
-
+  /**
+   * Calculate the costs of the company (including the salary of its employees)
+   * @param {Function(Error, number)} callback
+   */
+  Company.prototype.cost = function(cb) {
+    let filter = {};
+    this.employees(filter, (err, employeeResults) => {
+      if (err) return cb(err);
+      let totalCosts = employeeResults
+                      .map(emp => emp.salary)
+                      .reduce((salary, total) => total + salary);
+      cb(null, totalCosts);
+    });
+  };
 };
